b56ec5dd9454f28ec25ec2f7a336c5f7466a9fb1
mikepodolskiy <mikepodolskiy@gmail.com>
2023-03-26 17:25:52 +0300

    +validation of data and file existence, + short docstrings

--- app/views/view.py
+++ app/views/view.py
@@ -1,62 +1,62 @@
-import os
+# import required libraries and modules
 from flask import request, Blueprint, jsonify
+from marshmallow import ValidationError
+
 from app.funcs.functions import filter_data, map_data, read_file, sort_data, limit_data, unique_data
+from app.model.model import RequestSchema
 
 # creating blueprint
 main_blueprint = Blueprint('main_blueprint', __name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath('apache_logs.txt'))
-DATA_DIR = os.path.join(BASE_DIR, "data")
-
 
 @main_blueprint.route("/perform_query", methods=['POST'])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    data = request.json
+    # getting data from request
+    req_data = request.json
 
-    # input validation of data
-    # fieldnames, quantity etc.
-    cmd1 = data['cmd1']
-    value1 = data['value1']
-    cmd2 = data['cmd2']
-    value2 = data['value2']
-    # filter, map, unique, limit
+    # checking data correctness using marshmallow
+    try:
+        RequestSchema().load(req_data)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
 
-    data = read_file('data/apache_logs.txt')
+    # checking file existence, otherwise throwing exception
+    try:
+        data = read_file("".join(['data/', req_data['file_name']]))
+    except FileNotFoundError:
+        return 'File not found, check file path', 404
 
-    if cmd1 == 'filter':
-        output_data = filter_data(data, value1)
+    # check received data, applying relative function, forming data
+    if req_data['cmd1'] == 'filter':
+        output_data = filter_data(data, req_data['value1'])
 
-    elif cmd1 == 'map':
-        output_data = map_data(data, value1)
+    elif req_data['cmd1'] == 'map':
+        output_data = map_data(data, req_data['value1'])
 
-    elif cmd1 == 'sort':
-        output_data = sort_data(data, value1)
+    elif req_data['cmd1'] == 'sort':
+        output_data = sort_data(data, req_data['value1'])
 
-    elif cmd1 == 'limit':
-        output_data = limit_data(data, value1)
+    elif req_data['cmd1'] == 'limit':
+        output_data = limit_data(data, req_data['value1'])
 
-    elif cmd1 == 'unique':
+    elif req_data['cmd1'] == 'unique':
         output_data = unique_data(data)
 
-    # if cmd2:
+    # check second pair of received data, applying relative function to the previously formed data
+    if req_data['cmd2'] == 'filter':
+        output_data = filter_data(output_data, req_data['value2'])
 
-    if cmd2 == 'filter':
-        output_data = filter_data(output_data, value2)
+    elif req_data['cmd2'] == 'map':
+        output_data = map_data(output_data, req_data['value2'])
 
-    elif cmd2 == 'map':
-        output_data = map_data(output_data, value2)
+    elif req_data['cmd2'] == 'sort':
+        output_data = sort_data(output_data, req_data['value2'])
 
-    elif cmd2 == 'sort':
-        output_data = sort_data(output_data, value2)
+    elif req_data['cmd2'] == 'limit':
+        output_data = limit_data(output_data, req_data['value2'])
 
-    elif cmd2 == 'limit':
-        output_data = limit_data(output_data, value2)
-
-    elif cmd2 == 'unique':
+    elif req_data['cmd2'] == 'unique':
         output_data = unique_data(output_data)
 
+    # return result of function applying
     return jsonify(list(output_data)), 200
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import os
-
-from flask import Flask, request
-import requests
+from flask import Flask
 from app.views.view import main_blueprint
 
 # starting app
 app = Flask(__name__)
-# allow сyrillic symbols, setting configuration parameter for encoding
-app.config['JSON_AS_ASCII'] = False
 
 # registering blueprints
 app.register_blueprint(main_blueprint)
-
-
-
 
 # run app with import check
 if __name__ == '__main__':
